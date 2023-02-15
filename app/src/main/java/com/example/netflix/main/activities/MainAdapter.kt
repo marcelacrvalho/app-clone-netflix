9bafdf7a6e279e3bc3faeecdef806d47ab07d9fc
marcelacrvalho <marcelacrvalho@icloud.com>
2023-02-15 16:37:57 -0300

    Implementa as classes abstratas

--- app/src/main/java/com/example/netflix/main/activities/MainAdapter.kt
+++ app/src/main/java/com/example/netflix/main/activities/MainAdapter.kt
@@ -3,21 +3,29 @@ package com.example.netflix.main.activities
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.netflix.R
+import com.example.netflix.utils.models.Movie
 
-class MainAdapter: RecyclerView.Adapter<MainAdapter.MovieViewHolder>() {
+class MainAdapter(private val movies: List<Movie>): RecyclerView.Adapter<MainAdapter.MovieViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.movie_item, parent, false)
         return MovieViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return movies.size
     }
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
-        TODO("Not yet implemented")
+       val currentMovie = movies[position]
+        holder.bind(currentMovie)
     }
-    inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(movie: Movie) {
+            val tvMovie: TextView = itemView.findViewById(R.id.tv_movie)
+            tvMovie.text = movie.title
+        }
+    }
 }

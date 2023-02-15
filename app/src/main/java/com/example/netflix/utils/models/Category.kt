3a13ebba004e70945778905e019b6a350e9d5041
marcelacrvalho <marcelacrvalho@icloud.com>
2023-02-15 16:22:43 -0300

    Adiciona modelos Category e Movie

---
+++ app/src/main/java/com/example/netflix/utils/models/Category.kt
@@ -0,0 +1,3 @@
+package com.example.netflix.utils.models
+
+data class Category(val name: String, val movies: List<Movie>)
---
+++ app/src/main/java/com/example/netflix/utils/models/Movie.kt
@@ -0,0 +1,3 @@
+package com.example.netflix.utils.models
+
+data class Movie(val coverUrl: String, val title: String)

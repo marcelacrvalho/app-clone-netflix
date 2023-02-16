6d935db01da81c4e7bf604ce6195e3041763828f
marcelacrvalho <marcelacrvalho@icloud.com>
2023-02-16 10:31:29 -0300

    Muda a rolagem da Recycler View para horizontal

--- app/src/main/java/com/example/netflix/main/activities/MainActivity.kt
+++ app/src/main/java/com/example/netflix/main/activities/MainActivity.kt
@@ -2,21 +2,25 @@ package com.example.netflix.main.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.netflix.R
+import com.example.netflix.utils.models.Movie
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val movies = mutableListOf<Movie>()
+        for(i in 0 until 10) {
+            val movie = Movie("https://exemplo.com/${i}", "Titulo - $i")
+            movies.add(movie)
+        }
+
         val rv: RecyclerView = findViewById(R.id.rv_movie)
-        val adapter = MainAdapter()
-        rv.layoutManager = LinearLayoutManager(this)
+        val adapter = MainAdapter(movies)
+        rv.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
         rv.adapter = adapter
     }
 }

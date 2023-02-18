ccfbdaf52f049af3b38a6422cc4f18c3155cc720
marcelacrvalho <marcelacrvalho@icloud.com>
2023-02-18 14:57:17 -0300

    Instância ReciclerView de Category, inicializa layoutManager e adapter

--- app/src/main/java/com/example/netflix/main/activities/MainActivity.kt
+++ app/src/main/java/com/example/netflix/main/activities/MainActivity.kt
@@ -5,22 +5,18 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.netflix.R
-import com.example.netflix.utils.models.Movie
+import com.example.netflix.utils.models.Category
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val movies = mutableListOf<Movie>()
-        for(i in 0 until 10) {
-            val movie = Movie("https://exemplo.com/${i}", "Titulo - $i")
-            movies.add(movie)
-        }
+        val categories = mutableListOf<Category>()
 
-        val rv: RecyclerView = findViewById(R.id.rv_movie)
-        val adapter = MainAdapter(movies)
-        rv.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        val rv: RecyclerView = findViewById(R.id.rv_main)
+        val adapter = CategoryAdapter(categories)
+        rv.layoutManager = LinearLayoutManager(this)
         rv.adapter = adapter
     }
 }

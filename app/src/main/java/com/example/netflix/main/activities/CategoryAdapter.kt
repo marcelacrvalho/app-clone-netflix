1e5c29432dbef179d250d95d38fedbdf61097c27
marcelacrvalho <marcelacrvalho@icloud.com>
2023-02-18 14:55:45 -0300

    Cria layoutManager de CategoryAdapter e inicializa name e recyclerView

--- app/src/main/java/com/example/netflix/main/activities/CategoryAdapter.kt
+++ app/src/main/java/com/example/netflix/main/activities/CategoryAdapter.kt
@@ -4,11 +4,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.netflix.R
 import com.example.netflix.utils.models.Category
 
-class CategoryAdapter(private val categories: List<Category>): RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
+class CategoryAdapter(private val categories: MutableList<Category>): RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.category_item, parent, false)
         return CategoryViewHolder(view)
@@ -25,10 +26,11 @@ class CategoryAdapter(private val categories: List<Category>): RecyclerView.Adap
     inner class CategoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(category: Category) {
             val tvCategory: TextView = itemView.findViewById(R.id.tv_title)
-            tvCategory.text = category.title
+            tvCategory.text = category.name
 
             val rvCategory: RecyclerView = itemView.findViewById(R.id.rv_category)
-            rvCategory.
+            rvCategory.layoutManager = LinearLayoutManager(itemView.context, RecyclerView.HORIZONTAL, false)
+            rvCategory.adapter = MovieAdapter(category.movies)
         }
     }
 }

28d0ade40a9f6c0c142fdabad3ee643a55c9962a
marcelacrvalho <marcelacrvalho@icloud.com>
2023-02-19 13:26:47 -0300

    Seta o shadow criado e altera dinâmicamente a imagem

--- app/src/main/java/com/example/netflix/MovieActivity.kt
+++ app/src/main/java/com/example/netflix/MovieActivity.kt
@@ -1,8 +1,11 @@
 package com.example.netflix
 
+import android.graphics.drawable.LayerDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 
 class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +15,13 @@ class MovieActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val layerDrawable: LayerDrawable =
+            ContextCompat.getDrawable(this, R.drawable.shadows) as LayerDrawable
+        val movieCover = ContextCompat.getDrawable(this, R.drawable.image_1)
+        layerDrawable.setDrawableByLayerId(R.id.cover_drawable, movieCover)
+        val coverImg: ImageView = findViewById(R.id.iv_cover_movie)
+        coverImg.setImageDrawable(layerDrawable)
+
     }
 }
